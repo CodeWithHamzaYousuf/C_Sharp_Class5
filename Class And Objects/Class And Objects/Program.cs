a7953d17bf6193e44ce8092f274db2be72d179d4
Hamza Yousuf <156405574+CodeWithHamzaYousuf@users.noreply.github.com>
2025-01-29 12:45:35 +0500

    FIRST COMMIT

---
+++ Class And Objects/Class And Objects/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Class_And_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            //Person p1 = new Person();
+            //p1.firstName = "Hamza";
+            //p1.lastName = "Yousuf";
+            //p1.age = 20;
+            //Console.WriteLine(p1.GetFullName());
+
+            Car car1 = new Car();
+            car1.Enter_Details("Honda Civic", "Honda", 2025);
+            car1.details();
+        }
+    }
+
+    //class Person
+    //{
+
+    //    public string firstName;
+    //    public string lastName;
+    //    public int age;
+
+    //    public string GetFullName()
+    //    {
+    //        return $"Your full name is {this.firstName} {this.lastName} and age is {this.age}";
+    //    }
+    //}
+
+
+
+    class Car
+    {
+        public string model;
+        public string make;
+        public int year;
+
+        public void details()
+        {
+            Console.WriteLine($"your car details are {this.model} {this.make} {this.year}");
+        }
+
+        public void Enter_Details(string model, string make, int year)
+        {
+            this.model = model;
+            this.make = make;
+            this.year = year;
+        }
+    }
+
+
+
+
+}
